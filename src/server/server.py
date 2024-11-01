f14c38da1b28f711dd5f52626fa24326bde2d454
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2024-11-01 17:32:17 +0000

    Added NetTask protocol class.

--- src/server/server.py
+++ src/server/server.py
@@ -1,24 +1,27 @@
 import socket
 
-def start_udp_server_with_ack(host='10.0.0.10', port=38):
-    udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_server_socket.bind((host, port))
+class Server:
     
-    print(f"Servidor UDP está escutando em {host}:{port}")
-    
-    while True:
-        data, addr = udp_server_socket.recvfrom(1024)
-        message = data.decode()
-        print(f"Mensagem recebida de {addr}: {message}")
-        
-        # Extrai o número de sequência
-        if "Seq:" in message:
-            seq_num = message.split(":")[1].split(" - ")[0]
-            print(f"Número de sequência recebido: {seq_num}")
-            
-            # Envia um ACK de volta
-            ack_message = f"ACK:{seq_num}"
-            udp_server_socket.sendto(ack_message.encode(), addr)
+    def start_udp_server_with_ack(host='10.0.0.10', port=38):
+        udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_server_socket.bind((host, port))
+
+        print(f"Server listening at {host}:{port}")
+
+        while True:
+            data, addr = udp_server_socket.recvfrom(1024)
+            message = data.decode()
+            print(f"Mensagem recebida de {addr}: {message}")
+
+            # Extrai o número de sequência
+            if "Seq:" in message:
+                seq_num = message.split(":")[1].split(" - ")[0]
+                print(f"Número de sequência recebido: {seq_num}")
+
+                # Envia um ACK de volta
+                ack_message = f"ACK:{seq_num}"
+                udp_server_socket.sendto(ack_message.encode(), addr)
 
 if __name__ == "__main__":
-    start_udp_server_with_ack()
+    srv = Server()
+    srv.start_udp_server_with_ack()
